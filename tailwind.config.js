743ffcf7ccb257d62279304c8ba07413a0fc81de
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2024-09-19 18:28:13 +0200

    Configuracion inicial.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      extend: {
+        colors : [
+          { 
+            footer: '#263238',
+            body: '#EFF2FF'
+          }
+        ]
+      },
+    },
+    plugins: [],
+  };
